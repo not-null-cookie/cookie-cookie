da637200720d06f8f71808c1cbd235eb2dd09517
not-null-cookie <y.zzz@outlook.com>
2024-09-08 22:42:02 +0800

    second commit

--- cookie-sharp/src/index.ts
+++ cookie-sharp/src/index.ts
@@ -1,7 +1,11 @@
 import { Hono } from 'hono'
+import mime from "mime";
 
+/// <reference path="../worker-configuration.d.ts" />
+// 通过 npm run cf-typegen 更新配置
 const app = new Hono()
 
 app.get('/', (c) => c.text('Hello World!'))
+
 
 export default app
